4ede0af11061c9409eadbc6002ee7ebe01d423ce
rush <rush@rushs-Air.wv.cc.cmu.edu>
2015-01-31 18:40:09 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
+#import "CCPhysics+ObjectiveChipmunk.h"
 #import "Gameplay.h"
 
 @implementation Gameplay {
@@ -37,6 +38,8 @@
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
+    
+    _physicsNode.collisionDelegate = self;
 }
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
@@ -83,6 +86,32 @@
 {
     // when touches are cancelled, meaning the user drags their finger off the screen
     [self releaseCatapult];
+}
+
+- (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    float energy = [pair totalKineticEnergy];
+    // if energy is large enough, remove the seal
+    if (energy > 5000.f) {
+        // to ensure running the code block only per key and frame
+        [[_physicsNode space] addPostStepBlock:^{
+            [self sealRemoved:nodeA];
+        } key:nodeA];
+    }
+}
+
+- (void)sealRemoved:(CCNode *)seal
+{
+    // load particle effect
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    // make the particle effect clean itself up, once it is completed
+    explosion.autoRemoveOnFinish = YES;
+    // place the particle effect on the seals position
+    explosion.position = seal.position;
+    // add the particle effect to the same node that the seal is on
+    [self.parent addChild:explosion];
+    
+    [self removeFromParent];
 }
 
 - (void)releaseCatapult
