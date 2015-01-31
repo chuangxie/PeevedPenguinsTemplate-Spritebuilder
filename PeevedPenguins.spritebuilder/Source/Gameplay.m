7b50f30ce6f27cab502014adf8a0671691a0a602
rush <rush@rushs-Air.wv.cc.cmu.edu>
2015-01-31 14:43:43 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -23,7 +23,9 @@
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
-    NSLog(@"did load from ccb:gameplay");
+    
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = YES;
 }
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
