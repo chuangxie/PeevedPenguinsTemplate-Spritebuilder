912a993ca1ec765ca265e14ea24642b96cfca0b9
rush <rush@rushs-MacBook-Air.local>
2015-02-01 01:42:11 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -108,11 +108,6 @@ static const float MIN_SPEED = 5.f;
     }
 }
 
-- (void)ccPhysicsCollisionSeparate:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
-{
-    NSLog(@"....");
-}
-
 - (void)sealRemoved:(CCNode *)seal
 {
     // load particle effect
