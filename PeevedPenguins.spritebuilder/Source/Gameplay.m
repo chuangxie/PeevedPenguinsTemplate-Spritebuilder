feb27abb6bcd258195a292842a0686601f3edc52
rush <rush@rushs-MacBook-Air.local>
2015-02-01 02:01:49 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -111,7 +111,6 @@ static const float MIN_SPEED = 5.f;
     CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
     // make the particle effect clean itself up, once it is completed
     explosion.autoRemoveOnFinish = TRUE;
-    [explosion stopSystem];
     // place the particle effect on the seals position
     explosion.position = seal.position;
     // add the particle effect to the same node that the seal is on
